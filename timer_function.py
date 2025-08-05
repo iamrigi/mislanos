21335e2a7b6c7b56399c6795302c0c580f9429fa
iamrigi <156566150+iamrigi@users.noreply.github.com>
2025-08-05 17:32:28 +0530

    Delete timer_function.py

--- timer_function.py
+++
@@ -1,7 +0,0 @@
-import logging
-from archival_logic import archive_old_records
-
-def main(mytimer: func.TimerRequest) -> None:
-    logging.info("Archival function triggered.")
-    archive_old_records()
-    logging.info("Archival process completed.")
